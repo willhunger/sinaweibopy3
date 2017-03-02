5ed7a11fb72b09367a5eda8004f5ca1e537abf80
will <willhunger@foxmail.com>
2017-03-02 22:03:01 +0800

    refactoring request part

--- weibo.py
+++ weibo.py
@@ -19,6 +19,13 @@ def _obj_hook(paris):
         odict[str(key)] = value
     return odict
 
+def _encode_params(**kw):
+    'Encode parameters'
+    args = []
+    for key, value in kw.items():
+        para = value.encode('utf-8') if isinstance(value, str) else str(value)
+        args.append('%s=%s' % (key, urllib.parse.quote(para)))
+    return '&'.join(args)
 
 
 class JsonDict(dict):
@@ -31,13 +38,56 @@ class JsonDict(dict):
     def __setattr__(self, attr, value):
         self[attr] = value
 
+_HTTP_GET = 0
+_HTTP_POST = 1
+_HTTP_UPLOAD = 2
+
+def _http_get(url, authorization=None, **kw):
+    logging.info('GET %s' % url)
+    return _http_request(url, _HTTP_GET, authorization, **kw)
+
+def _http_post(url, authorization=None, **kw):
+    logging.info('POST %s' % url)
+    return _http_request(url, _HTTP_POST, authorization, **kw)
+
+def _http_upload(url, authorization=None, **kw):
+    logging.info('UPLOAD %s' % url)
+    return _http_request(url, _HTTP_UPLOAD, authorization, **kw)
+
+def _http_request(url, method, authorization, **kw):
+    'send http request and get json object'
+    params = None
+    boundary = None
+    if method == _HTTP_UPLOAD:
+        pass
+    else:
+        params = _encode_params(**kw)
+    http_url = '%s?%s' % (url, params) if method == _HTTP_GET else url
+    http_para = None if method == _HTTP_GET else params.encode(encoding='utf-8')
+    req = urllib.request.Request(http_url, data=http_para)
+    if authorization:
+        pass
+    if boundary:
+        pass
+    resq = urllib.request.urlopen(req)
+    body = resq.read().decode("utf-8")
+    result = json.loads(body, object_hook=_obj_hook)
+    if 'error_code' in result:
+        print('error')
+    return result
+
+
 
 class APIClient(object):
     'APIClient class'
-    def __init__(self, app_key, app_secret, redirect_uri):
+    def __init__(self, app_key, app_secret, redirect_uri=None,
+                 response_type='code', domain='api.weibo.com', version='2'):
         self.client_id = app_key
         self.client_secret = app_secret
         self.redirect_uri = redirect_uri
+        self.response_type = response_type
+        self.auth_url = 'https://%s/oauth2/' % domain
+        self.api_url = 'https://%s/%s/' % (domain, version)
         self.access_token = None
         self.expires = 0.0
 
@@ -49,20 +99,11 @@ code&client_id=%s&redirect_uri=%s'''%(self.client_id, self.redirect_uri)
 
     def request_access_token(self, code):
         """post a request and then get a access_token"""
-        para = {
-            "client_id" : self.client_id,
-            "client_secret" : self.client_secret,
-            "redirect_uri" : self.redirect_uri,
-            "code" : code,
-            "grant_type" : 'authorization_code'
-            }
-        data = urllib.parse.urlencode(para)
-        data = data.encode(encoding='utf-8')
-        request = urllib.request.Request("https://api.weibo.com/oauth2/access_token", data=data)
-        res = urllib.request.urlopen(request)
-        resq = res.read().decode("utf-8")
-        result = json.loads(resq, object_hook=_obj_hook)
-        logging.info(result)
+        result = _http_post('%s%s' % (self.auth_url, 'access_token'), \
+                          client_id=self.client_id, \
+                          client_secret=self.client_secret, \
+                          redirect_uri=self.redirect_uri, \
+                          code=code, grant_type='authorization_code')
         result.expires_in += int(time.time())
         return result
 
@@ -73,12 +114,20 @@ code&client_id=%s&redirect_uri=%s'''%(self.client_id, self.redirect_uri)
         self.expires = float(expires_in)
 
     def public_timeline(self):
-        'get new public weibo'
-        url = 'https://api.weibo.com/2/statuses/public_timeline.json?access_token=%s'%(self.access_token)
-        request = urllib.request.Request(url)
-        res = urllib.request.urlopen(request)
-        resq = res.read().decode("utf-8")
-        result = json.loads(resq, object_hook=_obj_hook)
+        '''
+        get new public weibo,the parameters followed can be used in _http_get in this method
+        access_token : (string) the token you got after OAuth
+        count : (int) the record items in one single page,default 50 items
+        page : (int) the page number,default one page
+        base_app : (int) whether get data in current app or not,
+            0 is not(all data),1 is yes(current app),default 0
+        '''
+        result = _http_get('%s'% (self.api_url)  + 'statuses/public_timeline.json', \
+                           access_token=self.access_token, \
+                           count=50, \
+                           page=1, \
+                           base_app=0, \
+                )
         return result
 
 
