6db6a31160248d102eeb8de72d2c9418013883d5
will <willhunger@foxmail.com>
2017-03-02 13:10:41 +0800

    fix url problems

--- weibo.py
+++ weibo.py
@@ -43,8 +43,8 @@ class APIClient(object):
 
     def get_authorize_url(self):
         'get authorize url'
-        return '''https://api.weibo.com/oauth2/authorize?response_type='
-        code&client_id=%s&redirect_uri=%s'''%(self.client_id, self.redirect_uri)
+        return '''https://api.weibo.com/oauth2/authorize?response_type=\
+code&client_id=%s&redirect_uri=%s'''%(self.client_id, self.redirect_uri)
 
 
     def request_access_token(self, code):
@@ -72,6 +72,14 @@ class APIClient(object):
         self.access_token = str(access_token)
         self.expires = float(expires_in)
 
+    def public_timeline(self):
+        'get new public weibo'
+        url = 'https://api.weibo.com/2/statuses/public_timeline.json?access_token=%s'%(self.access_token)
+        request = urllib.request.Request(url)
+        res = urllib.request.urlopen(request)
+        resq = res.read().decode("utf-8")
+        result = json.loads(resq, object_hook=_obj_hook)
+        return result
 
 
 def main():
@@ -87,9 +95,10 @@ def main():
         client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=REDIRECT_URL)
         print(client.get_authorize_url())
         #step 3 : get Access Token
-        result = client.request_access_token(input("please input code"))
+        result = client.request_access_token(input("please input code : "))
         client.set_access_token(result.access_token, result.expires_in)
         #step 4 : using api by access_token
+        print(client.public_timeline())
     except ValueError:
         print('pyOauth2Error')
 
